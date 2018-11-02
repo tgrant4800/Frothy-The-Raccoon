c3b6d36c44911b92767e9e3f3f8a4e0ab418a477
Timothy <44624515+tgrant4800@users.noreply.github.com>
2018-11-02 18:30:31 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,25 +7,16 @@ import time
 Client = discord.Client()
 client = commands.Bot(command_prefix = "?")
 
-chat_filter = ["TRASH", "GARBAGE"]
-bypass_list = []
-
 @client.event
 async def on_ready():
     print("Bot is online and connected to Discord")
 
 @client.event
 async def on_message(message):
-    contents = message.content.split(" ") #contents is a list type
-    for word in contents:
-        if word.upper() in chat_filter:
-            if not message.author.id in bypass_list:
-                try:
-                    await client.send_message(message.channel, "Did somebody say TRASH??")
-                except discord.errors.NotFound:
-                    return
-                   
-                    
-        
+    if client.user.id != message.author.id:
+        if 'trash' in message.content:
+            await client.send_message(message.channel, 'Did somebody say TRASH??')
 
+    await client.process_commands(message)
+    
 client.run("NTA2OTY0NzcyNzY0NTE2MzUy.Dr5nrg.GcYJS42adX6wwVgYGjXq4P-JBio")
