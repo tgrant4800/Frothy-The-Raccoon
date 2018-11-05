bb74699a720a074699394332cfafe8887bd20df5
Timothy <44624515+tgrant4800@users.noreply.github.com>
2018-11-05 17:08:27 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,22 +1,19 @@
-import discord
-from discord.ext.commands import Bot
 from discord.ext import commands
-import asyncio
-import time
-
-Client = discord.Client()
-client = commands.Bot(command_prefix = "?")
-
+ 
+ 
+client = commands.Bot(command_prefix='?')
+ 
+ 
 @client.event
 async def on_ready():
-    print("Bot is online and connected to Discord")
-
+    print('Bot is online and connected to Discord')
+ 
+ 
 @client.event
 async def on_message(message):
     if client.user.id != message.author.id:
         if 'trash' in message.content:
-            await client.send_message(message.channel, 'Did somebody say TRASH??')
-
-    await client.process_commands(message)
-    
-client.run("")
+            await message.channel.send('Did somebody say TRASH??')
+ 
+ 
+client.run('NTA2OTY0NzcyNzY0NTE2MzUy.DsJe3g.hGXE78_OcE-ZeNaCGG5SMO-gd70')
