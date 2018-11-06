8446361bcfa40f6435efdd869c904c7c0010f52c
Timothy <44624515+tgrant4800@users.noreply.github.com>
2018-11-06 17:45:28 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -26,4 +26,4 @@ async def on_message(message):
         if 'communism' in message.content.lower():
             await message.channel.send('слава советской россии')
  
-client.run('NTA2OTY0NzcyNzY0NTE2MzUy.DsJz_Q.Lf8kO_T_kehlMtBTaWgBayqwn4Y')
+client.run('NTA2OTY0NzcyNzY0NTE2MzUy.DsO4xQ.F-zJX1qw7a-EJRyPh516sJ-0UhQ')
