7ff40b56b17bf62fbb3d5c679c464bc263613e84
Timothy <44624515+tgrant4800@users.noreply.github.com>
2018-11-05 19:34:30 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,5 +16,14 @@ async def on_message(message):
     if client.user.id != message.author.id:
         if 'book' in message.content.lower():
             await message.channel.send('jaZ$GMBFUvetbmSER(PG&%Y*T&*GFNYGP%*NBGSUVEnGUSBN(REYbyfinvdfvaWERBSGFHAsDFBGHyJ7TYbrEvcgbTRsEBRgC *Frothy Ran Away*')
+    if client.user.id != message.author.id:
+        if 'garbage' in message.content.lower():
+            await message.channel.send('{0.author.mention} is garbage.')
+    if client.user.id != message.author.id:
+        if 'communist' in message.content.lower():
+            await message.channel.send('слава советской россии')
+    if client.user.id != message.author.id:
+        if 'communism' in message.content.lower():
+            await message.channel.send('слава советской россии')
  
 client.run('NTA2OTY0NzcyNzY0NTE2MzUy.DsJz_Q.Lf8kO_T_kehlMtBTaWgBayqwn4Y')
