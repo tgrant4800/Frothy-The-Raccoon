6e5b08ac8e187808c30a33b3827c77f136491ed6
Timothy <44624515+tgrant4800@users.noreply.github.com>
2018-11-05 19:04:07 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,10 +11,10 @@ async def on_ready():
 @client.event
 async def on_message(message):
     if client.user.id != message.author.id:
-        if 'trash' in message.content:
+        if 'trash' in message.content.lower():
             await message.channel.send('Did somebody say TRASH??')
     if client.user.id != message.author.id:
-        if 'book' in message.content:
+        if 'book' in message.content.lower():
             await message.channel.send('jaZ$GMBFUvetbmSER(PG&%Y*T&*GFNYGP%*NBGSUVEnGUSBN(REYbyfinvdfvaWERBSGFHAsDFBGHyJ7TYbrEvcgbTRsEBRgC *Frothy Ran Away*')
  
 client.run('NTA2OTY0NzcyNzY0NTE2MzUy.DsJz_Q.Lf8kO_T_kehlMtBTaWgBayqwn4Y')
